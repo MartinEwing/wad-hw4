f80d83598708a5bd004936fd69f040148cc27be9
MartinEwing <mart99in.server@gmail.com>
2019-12-15 18:08:51 +0200

    Made a performance improvement

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -4,14 +4,13 @@ import Item from '../../src/models/Item.js'
 
 describe('Task 1 tests', ()=> {
 
+    const wrapper = mount(List)
+
     it('Text is shown when empty',()=>{
-        const wrapper = mount(List)
         expect(wrapper.html()).toContain('Add your first Todo task')
     })
 
     it('When an item in the list is marked as done item is updated correctly',async ()=>{
-        const wrapper = mount(List)
-
         let emptyList = []
         expect(wrapper.props().list).toEqual(emptyList) // Empty list
 
